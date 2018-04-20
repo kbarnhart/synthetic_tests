619708f4b52391af98b6de37c740f7d6b330624d
kbarnhart <katy.barnhart@gmail.com>
2018-04-20 11:38:54 -0500

    initial commit of noise in K

---
+++ p1_recovery_with_known_initial_condition/of_timing_and_transport/with_noise_in_K/start_dakota_summit_truth.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH --job-name synthetic_test
+#SBATCH --ntasks-per-node 24
+#SBATCH --partition shas
+#SBATCH --mem-per-cpu 4GB
+#SBATCH --nodes 1
+#SBATCH --time 10:00:00
+
+# load environment modules
+module load intel/16.0.3
+module load openmpi/1.10.2
+module load cmake/3.5.2
+#module load perl
+module load mkl
+module load gsl
+
+# make sure environment variables are set correctly
+source ~/.bash_profile
+## run dakota using a restart file if it exists.
+if [ -e dakota.rst ]
+then
+  dakota -i dakota_hierarchical.in -o dakota_hierarchical.out --read_restart dakota.rst
+else
+  dakota -i dakota_hierarchical.in -o dakota_hierarchical.out
+fi

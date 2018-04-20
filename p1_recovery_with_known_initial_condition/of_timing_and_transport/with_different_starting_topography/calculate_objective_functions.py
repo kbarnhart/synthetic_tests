8eadb1a375dc115abf85c4bbe3176592d79c603a
kbarnhart <katy.barnhart@gmail.com>
2018-04-20 12:45:33 -0500

    of calc and plots for different starting topography

---
+++ p1_recovery_with_known_initial_condition/of_timing_and_transport/with_different_starting_topography/calculate_objective_functions.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 19:13:39 2018
+
+@author: barnhark
+"""
+
+# different starting topo
+
+import glob
+import os
+import numpy as np
+
+from joblib import Parallel, delayed
+
+from yaml import load
+
+import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+from scipy.interpolate import interp1d
+
+pattern = 'RESULTS/create_truth.*/truth_profile.csv'
+files = np.sort(glob.glob(pattern))
+    
+def calculate_objective_function(file):
+    print(file)
+    
+    pattern = 'RESULTS/create_truth.*/truth_profile.csv'
+    files = glob.glob(pattern)
+
+    folder = os.path.split(file)[0]
+    run = int(folder.split('.')[-1])
+
+    if os.path.exists(folder+os.path.sep + 'CALIB') == False:
+        os.mkdir(folder+os.path.sep + 'CALIB')
+
+    with open(folder+os.path.sep + 'inputs.txt', 'r') as f:
+        inputs = load(f)
+        Pp_truth = inputs['process_parameter']
+        Fd_truth = inputs['faulting_duration']
+        
+    df_truth = pd.read_csv(file)
+    
+    df_list = []
+       
+    for data_file in files:
+        
+        data_folder = os.path.split(data_file)[0]
+        data_run = int(data_folder.split('.')[-1])
+        
+        with open(data_folder + os.path.sep + 'inputs.txt', 'r') as f:
+            data_inputs = load(f)
+            Pp_data = data_inputs['process_parameter']
+            Fd_data = data_inputs['faulting_duration']
+        
+        data_frame = pd.read_csv(data_file)
+    
+    
+        interp_ob = interp1d(np.log10(data_frame.area), np.log10(data_frame.slope), bounds_error=False, fill_value=np.nan)
+        log_interpolated_slope = interp_ob(np.log10(df_truth.area))
+        
+        ssd = np.nansum((np.log10(df_truth.slope) - log_interpolated_slope)**2.0)
+        
+        
+        df_list.append({'run': data_run,
+                        'process_parameter': Pp_data,
+                        'faulting_duration': Fd_data,
+                        'objective_function': ssd})
+    
+ 
+#        fs = (8, 6)
+#        fig, ax = plt.subplots(figsize=fs, dpi=300)
+#        plt.plot(df_truth.area, df_truth.slope, label='Truth')
+#
+#        plt.plot(data_frame.area, data_frame.slope, label='Run')
+#        plt.legend()
+#        ax.set_xscale('log')
+#        ax.set_yscale('log')
+#        plt.xlabel('log 10 Area')
+#        plt.ylabel('log 10 Slope')
+#        
+#        plt.text(4e5, 1e-2, ('Truth:\nPp = ' + str(np.round(Pp_truth, 2)) + '\n'
+#                             'Fd = ' + str(np.round(Fd_truth, 2)) + '\n\n'
+#                             'Run :\nPp = ' + str(np.round(Pp_data, 2)) + '\n'
+#                             'Fd = ' + str(np.round(Fd_data, 2)) + '\n'))
+#                             
+#        plt.savefig(folder+os.path.sep+'CALIB/obj_func.' + str(data_run) + '.png')
+#        plt.close()
+      
+    df_out = pd.DataFrame(df_list)
+    df_out.set_index('run', inplace=True)
+    df_out.sort_index(axis=0, inplace=True)
+    
+    df_out.to_csv(folder + os.path.sep + 'calib_results.csv')
+#%%
+output = Parallel(n_jobs=5)(delayed(calculate_objective_function)(file) for file in files)
+#for file in files:
+#    calculate_objective_function(file)
---
+++ p1_recovery_with_known_initial_condition/of_timing_and_transport/with_different_starting_topography/make_plots.py
@@ -0,0 +1,70 @@
+import glob
+import os
+import numpy as np
+
+from yaml import load
+
+import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')
+
+
+import matplotlib.pyplot as plt
+
+
+pattern = 'RESULTS/create_truth.*/calib_results.csv'
+
+out = 'figures'
+
+size = 41
+
+if os.path.exists(out) == False:
+    os.mkdir(out)
+
+
+files = np.sort(glob.glob(pattern))
+for file in files:
+    folder = os.path.split(file)[0]
+    print(folder)
+    with open(folder+os.path.sep+'inputs.txt', 'r') as f:
+        inputs = load(f)
+    df = pd.read_csv(file)
+
+    X = df.process_parameter.values.reshape((size,size))
+    Y = df.faulting_duration.values.reshape((size,size))
+    dx = np.diff(X).mean()
+    dy = np.diff(Y, axis=0).mean()
+
+    Xplot = np.empty((size+1, size+1))
+    Yplot = np.empty((size+1, size+1))
+    Yplot[:size, :size] = Y-(0.5*dy) # shift by half a grid cell, so center of each square plots in correct place
+    Xplot[:size, :size] = X-(0.5*dx) # shift by half a grid cell, so center of each square plots in correct place
+    Yplot[:size,-1] = Yplot[:size,-2]
+    Xplot[-1, :size] = Xplot[-2, :size]
+    Yplot[size,:] = Y.max() + 0.5*dy
+    Xplot[:, size] = X.max() + 0.5*dx
+
+    OF = df['objective_function'].values.reshape((size,size))
+    fs = (8, 6)
+    
+    
+    fig, ax = plt.subplots(figsize=fs, dpi=300)
+    plt.pcolormesh(Xplot,Yplot, OF, cmap='gist_earth_r', vmin=0, vmax=6)
+    plt.colorbar(label='Objective Function')
+    plt.plot(inputs['process_parameter'], inputs['faulting_duration'], 'r*', label='Correct Value')
+    plt.xlabel('$\\mathit{Transport}$ $\\mathit{Limited}\\longleftarrow$   Process Parameter   $\\longrightarrow \\mathit{Detachment}$ $\\mathit{Limited}$')
+    plt.ylabel('Faulting Duration (MA)')
+    figname = 'Pp_'+str(inputs['process_parameter'])+'.Fd_'+ str(inputs['faulting_duration'])+ '.OF_abs.png'
+    plt.savefig(out+os.path.sep+figname)
+    plt.close()
+
+    fig, ax = plt.subplots(figsize=fs, dpi=300)
+    plt.pcolormesh(Xplot,Yplot, np.log10(OF), cmap='viridis_r', vmin=-3, vmax=2)
+    plt.colorbar(label='Log10(Objective Function)')
+    plt.plot(inputs['process_parameter'], inputs['faulting_duration'], 'r*', label='Correct Value')
+    plt.xlabel('$\\mathit{Transport}$ $\\mathit{Limited}\\longleftarrow$   Process Parameter   $\\longrightarrow \\mathit{Detachment}$ $\\mathit{Limited}$')
+    plt.ylabel('Faulting Duration (MA)')
+    figname = 'Pp_'+str(inputs['process_parameter'])+'.Fd_'+ str(inputs['faulting_duration'])+ '.OF_log.png'
+    plt.savefig(out+os.path.sep+figname)
+    plt.close()
